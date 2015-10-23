3db80c54fcf0d48f8f0d082ef9156e088d1ad4cf
txiang2 <timothy.xiang@gmail.com>
2015-10-23 18:48:24 -0500

    clean
    made it more clean

--- Danger/Web2js.js
+++ Danger/Web2js.js
@@ -1,29 +1,10 @@
 var main = function() {
-	/*var flash = "#FF0000";
-	var flash2 = "#00FFFF";
-	var hold = "";
-	var epil = function(a) {
-		document.getElementById("upload").style.backgroundColor = a; };
-		/*var x = b;
-		var y = a;
-		setTimeout(epil(x, y), 100);
-	}
-	for (var x = 0; x < 111; x++){
-		setTimeout(epil(flash), 100);
-		hold = flash;
-		flash = flash2;
-		flash2 = hold;
-	}*/
 	function epil() {
-		//var c = document.getElementById("upt");
-		//c.style.backgroundColor = "#C00000";
 		$('.back').addClass('red');
 		$('.back').removeClass('blue');
 		setTimeout(function() {apil();}, 10);
 	};
 	function apil() {
-		//var c = document.getElementById("upt");
-		//c.style.backgroundColor = "#0000C0";
 		$('.back').addClass('blue');
 		$('.back').removeClass('red');
 		setTimeout(function() {epil();}, 10);
@@ -51,13 +32,4 @@ $(document).ready(main);
 
 function something() {
 	alert("SOMETHING");
-	/*var flash = "#FF0000";
-	var flash2 = "#00FFFF";
-	var epil = function(a, b) {
-		document.getElementById("upload").style.backgroundColor = a;
-		var x = b;
-		var y = a;
-		setTimeout(epil(x, y), 100);
-	}
-	epil(flash, flash2);*/
 };
