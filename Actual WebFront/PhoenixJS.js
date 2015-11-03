c5ff9b82050f0653b9b33b3b78a370fc5c117ade
txiang2 <timothy.xiang@gmail.com>
2015-11-02 18:34:18 -0600

    MOVING NOTES :O

--- Actual WebFront/PhoenixJS.js
+++ Actual WebFront/PhoenixJS.js
@@ -1,16 +1,33 @@
-//This does nothing right now, I'm trying to get an image to
-//shoot across the screen periodically.
-
 var main = function() {
-  var a = document.createElement("img");
-  a.src = "http://cliparts.co/cliparts/8T6/oLr/8T6oLrK8c.png";
-  a.height = 100;
-  a.width = 100;
-}
+  function res(note) {
+    $("#" + note).removeClass('show');
+    $("#" + note).animate({left: '0%'}, 0);
+    dance(note);
+  };
+
+  function dance(note) {
+    var line = Math.floor(9 * Math.random()) * 10;
+    $("#" + note).animate({top: "-" + line + "px"}, 0);
+    $("#" + note).addClass('show');
+    $("#" + note).animate({left: '100%'}, 7000, "linear");
+    setTimeout(function() {res(note);}, 7000);
+  };
+  dance("n0");
+  setTimeout(function() {dance("n1")}, 1000);
+  setTimeout(function() {dance("n2")}, 2000);
+  setTimeout(function() {dance("n3")}, 3000);
+  setTimeout(function() {dance("n4")}, 4000);
+  setTimeout(function() {dance("n5")}, 5000);
+  setTimeout(function() {dance("n6")}, 6000);
+};
 
 $(document).ready(main);
 
 function upload() {
-	var file = document.getElementById("FileUpload").value;
-	alert("File: " + file);
-}
+	alert("File: kjbbj");
+};
+
+function play(n) {
+	var notefile = n + '.mp3';
+	notefile.play();
+};
